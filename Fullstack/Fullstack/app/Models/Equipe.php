42ad9a16c89c02378373266a049a96605fb8051e
Youssef <youssef.mhenni@esprit.tn>
2020-11-19 17:09:38 +0100

    ajouter fonction in Model Equipe
    Relation réflexive (manytomany)

--- Fullstack/Fullstack/app/Models/Equipe.php
+++ Fullstack/Fullstack/app/Models/Equipe.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Equipe extends Model
 {
     use HasFactory;
+
+    public function equipes1() {
+        return $this->belongsToMany(Equipe::class ,'matches', 'equipe_locaux_id','equipe_visiteurs_id');
+
+    }
+    public function equipes2() {
+        return $this->belongsToMany(Equipe::class ,'matches', 'equipe_visiteurs_id','equipe_locaux_id');
+
+    }
 }
