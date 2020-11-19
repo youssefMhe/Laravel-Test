6ced0ec5eee9cdb093bd5ce0f958b7d555e2014e
Youssef <youssef.mhenni@esprit.tn>
2020-11-19 20:12:54 +0100

    afficher listes des matchs et equipes

--- Fullstack/Fullstack/resources/views/equipe/index.blade.php
+++ Fullstack/Fullstack/resources/views/equipe/index.blade.php
@@ -4,4 +4,30 @@
 
     <h1 class="font-weight-bold">Liste des Equipes </h1>
 
+
+    <table class="table table-striped table-dark">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">created at</th>
+            <th scope="col">update at </th>
+            <th scope="col">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach(\App\Models\Equipe::all() as $equipe)
+        <tr>
+            <th scope="row">{{$equipe->id}}</th>
+            <td>{{$equipe->nom}}</td>
+            <td>{{$equipe->created_at}}</td>
+            <td>{{$equipe->updated_at}}</td>
+            <td>@fat</td>
+        </tr>
+        @endforeach
+
+        </tbody>
+    </table>
+
 @endsection
--- Fullstack/Fullstack/resources/views/match/index.blade.php
+++ Fullstack/Fullstack/resources/views/match/index.blade.php
@@ -4,4 +4,33 @@
 
     <h1 class="font-weight-bold">Liste des Matches </h1>
 
+    <table class="table table-striped table-dark">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Score</th>
+            <th scope="col">id equipe locaux</th>
+            <th scope="col">id equipe visiteurs</th>
+            <th scope="col">created at</th>
+            <th scope="col">update at </th>
+            <th scope="col">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @foreach(\App\Models\Match::all() as $match)
+            <tr>
+                <th scope="row">{{$match->id}}</th>
+                <td>{{$match->score}}</td>
+                <td>{{$match->equipe_locaux_id}}</td>
+                <td>{{$match->equipe_visiteurs_id }}</td>
+                <td>{{$match->created_at}}</td>
+                <td>{{$match->updated_at}}</td>
+                <td>@fat</td>
+            </tr>
+        @endforeach
+
+        </tbody>
+    </table>
+
 @endsection
