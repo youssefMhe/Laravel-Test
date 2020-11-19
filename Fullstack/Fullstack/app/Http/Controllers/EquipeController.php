47adcaaa920a3f07c94b77bee60e33cfd37f1993
Youssef <youssef.mhenni@esprit.tn>
2020-11-19 15:59:40 +0100

    Creation du projet Laravel
    creation du projet Laravel  avec un 1er  model  ' Equipe'

---
+++ Fullstack/Fullstack/app/Http/Controllers/EquipeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equipe;
+use Illuminate\Http\Request;
+
+class EquipeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Equipe  $equipe
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Equipe $equipe)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Equipe  $equipe
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Equipe $equipe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Equipe  $equipe
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Equipe $equipe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Equipe  $equipe
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Equipe $equipe)
+    {
+        //
+    }
+}
