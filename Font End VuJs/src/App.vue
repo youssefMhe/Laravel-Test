6427feb7ce629474f10e344375894e444b1cb98a
Youssef <youssef.mhenni@esprit.tn>
2020-11-21 01:23:19 +0100

    affichage du liste des equipe avec REST API
    use RESt API to show all Equipe

--- Font End VuJs/src/App.vue
+++ Font End VuJs/src/App.vue
@@ -2,18 +2,19 @@
     <div id="app">
         <router-view name="header"></router-view>
         <main>
-            <fade-transition origin="center" mode="out-in" :duration="250">
-                <router-view/>
-            </fade-transition>
+
+          <EquipeListe/>
         </main>
         <router-view name="footer"></router-view>
     </div>
 </template>
 <script>
 import { FadeTransition } from "vue2-transitions";
+import EquipeListe from "@/components/EquipeListe";
 
 export default {
   components: {
+    EquipeListe,
     FadeTransition
   }
 };
---
+++ Font End VuJs/src/components/EquipeListe.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1> Liste des Equipes </h1>
+    <table border="1px" class="table table-dark">
+      <tr>
+        <td> Nom</td>
+        <td> Created_at</td>
+        <td> Updated_at</td>
+      </tr>
+      <tr v-for="item in list" v-bind:key="item.id">
+        <td>{{item.nom}}</td>
+        <td>{{item.created_at}}</td>
+        <td>{{item.updated_at}}</td>
+
+      </tr>
+
+    </table>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios';
+Vue.use(VueAxios,axios)
+export default {
+  name:"EquipeListe",
+  data(){
+    return{list:undefined}
+  },
+  mounted() {
+    Vue.axios.get('http://127.0.0.1:8000/api/ListEquipe').then( (resp)=>{
+      this.list =resp.data
+      console.warn(resp)
+    })
+}
+}
+</script>
